da6692fd41ab6598e982bd563e0be243c88a09f7
Nataly <natalya-kotenkova@yandex.ru>
2022-12-05 16:02:39 +0100

    added task25

---
+++ task25/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear(); 
+Console.Write("Ведите число A: "); 
+int A = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Ведите число B: "); 
+int B = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 1 ; i <= B; i++)
+result = (A * A) ;
+Console.Write($" Результат: {result} ");
