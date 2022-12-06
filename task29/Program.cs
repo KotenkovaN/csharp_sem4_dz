8e2fb2c3e4a75d565f8e5f60b60c54341666b449
Nataly <natalya-kotenkova@yandex.ru>
2022-12-06 14:12:04 +0100

    added task29

---
+++ task29/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear(); 
+int[] array = new int[8]; 
+for (int i = 0; i < 8; i++) 
+{ 
+          Console.Write("Введите элемент массива: "); 
+          int x = Convert.ToInt32(Console.ReadLine()); 
+          array[i] = x; 
+} 
+Console.WriteLine($"[{string.Join(", ", array)}]"); 
